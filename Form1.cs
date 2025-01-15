12007111eb6efcc7a4023b0178eeb2a1c9324e0b
ok230193 <OK230193@oks.iput.ac.jp>
2025-01-15 12:50:21 +0900

    opencvでノートPC内臓のカメラを表示する

--- Form1.cs
+++ Form1.cs
@@ -4,44 +4,57 @@ using System.Windows.Forms;
 using OpenCvSharp;
 using OpenCvSharp.Extensions;
 
-
-namespace WinFormsApp2
+namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
-
-        private PictureBox PictureBox1;
+        private VideoCapture capture; // カメラキャプチャ用変数
+        private System.Windows.Forms.Timer timer; // フレーム更新用タイマー用変数
+        private PictureBox pictureBox1; // 映像表示用PictureBox用変数
 
         public Form1()
         {
             InitializeComponent();
 
-
-            PictureBox1 = new PictureBox()
+            // PictureBoxの設定
+            pictureBox1 = new PictureBox
             {
-                Location = new System.Drawing.Point(10, 10),
-                Size = new System.Drawing.Size(800, 600),
-                BorderStyle = BorderStyle.Fixed3D,
+                Location = new System.Drawing.Point(1, 1),
+                Size = new System.Drawing.Size(900, 600),
                 SizeMode = PictureBoxSizeMode.Zoom
             };
+            Controls.Add(pictureBox1);
 
-            this.Controls.Add(PictureBox1); //this = Form1
-
-            this.Load += Form1_Load;
+            // Loadイベントでカメラを初期化　　Loadで画面表示(表示させるために先に定義)、Form1_Loadで表示した後の行動(動き)=(表示と動きの処理は別で考える)
+            Load += Form1_Load;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string filePath = "example.jpg";
+            capture = new VideoCapture(0); // 内蔵カメラを初期化
 
-            using (Mat Image = new Mat(filePath))
-            {
-                PictureBox1.Image = BitmapConverter.ToBitmap(Image);
-            }
-
+            // タイマーを設定して映像を更新
+            timer = new System.Windows.Forms.Timer(); // Windows Forms用のタイマー
+            timer.Interval = 30; // フレーム更新間隔（約30FPS）
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            using (Mat frame = new Mat())//usingを使うことで{}内のみでframeを使えるようにし、{}を抜けると消える。
+            {
+                capture.Read(frame); // カメラからフレームを取得
+                pictureBox1.Image = BitmapConverter.ToBitmap(frame); // PictureBoxに表示
+            }
+        }
 
-
+        protected override void OnFormClosed(FormClosedEventArgs e) //終了時に自動で起動
+        {
+            // リソースを解放
+            timer.Stop();
+            capture.Release();
+            capture.Dispose();
+        }
     }
 }
